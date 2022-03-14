747b88a5759890fe6811282f34f7990dfe3cbe7c
Uriel Benítez Medina <urielbenitez96@hotmail.com>
2022-03-13 23:12:38 -0600

    use data api

--- src/components/ItemBlog/ItemBlog.jsx
+++ src/components/ItemBlog/ItemBlog.jsx
@@ -1,34 +1,35 @@
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
 import { Link } from "react-router-dom";
 import "./ItemBlog.scss";
-const ItemBlog = () => {
+const ItemBlog = ({ article }) => {
+  const { id, img, title, date, autor, shortDesc } = article;
   return (
     <div className="itemBlog">
       <img
         className="blogImage"
         loading="lazy"
-        src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        src={img}
         alt="logo"
         width="120px"
         height="60px"
       />
       <div className="about">
-        <h2 className="blogTitle">Titulo de la entrada</h2>
+        <h2 className="blogTitle">{title}</h2>
         <div className="metaData">
           <p className="date">
-            Escrito el:<span className="data"></span>
+            Escrito el:
+            <span className="data">
+              {format(new Date(date), "do-MMM-YYY", { locale: es })}
+            </span>
           </p>
           <p className="author">
-            por:<span className="data"></span>
+            por:<span className="data">{autor}</span>
           </p>
         </div>
-        <p className="aboutText">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur
-          reprehenderit, architecto rem culpa odit adipisci facilis at aut!
-          Dolor inventore commodi mollitia eius ducimus in quos delectus dicta
-          dignissimos debitis? Lorem ipsum dolor sit, amet consectetur
-        </p>
+        <p className="aboutText">{shortDesc}</p>
       </div>
-      <Link to={`/articulo/${1}`} className="ActionButton">
+      <Link to={`/articulo/${id}`} className="ActionButton">
         Ver Entrada
       </Link>
     </div>
