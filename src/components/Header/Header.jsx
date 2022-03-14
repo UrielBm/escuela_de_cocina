ea36431b2ad9e3f890ad38a191eae65febca4e0c
Uriel Benítez Medina <urielbenitez96@hotmail.com>
2022-03-13 23:11:47 -0600

    add link home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,23 @@ import logowebp from "./../../assets/img/logo.webp";
 import logo from "./../../assets/img/logo.png";
 import "./Header.scss";
 import Nav from "../Nav/Nav";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="wrapper header">
-      <picture>
-        <source srcSet={logowebp} type="image/webp" />
-        <img
-          className="logo"
-          loading="lazy"
-          src={logo}
-          alt="logo"
-          width="400px"
-          height="200px"
-        />
-      </picture>
+      <Link to="/">
+        <picture>
+          <source srcSet={logowebp} type="image/webp" />
+          <img
+            className="logo"
+            loading="lazy"
+            src={logo}
+            alt="logo"
+            width="400px"
+            height="200px"
+          />
+        </picture>
+      </Link>
       <Nav />
     </header>
   );
